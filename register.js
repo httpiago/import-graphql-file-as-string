74f8c4f080fe3f6d548131b4409ef84b8c127748
Iago Bruno <httpiago@gmail.com>
2019-09-04 21:10:13 -0300

    Update register.js

--- register.js
+++ register.js
@@ -1,4 +1,3 @@
-const { parse } = require('graphql');
 const { readFileSync } = require('fs');
 
 const VALID_EXTENSIONS = ['graphql', 'graphqls', 'gql', 'gqls'];
@@ -6,7 +5,7 @@ const VALID_EXTENSIONS = ['graphql', 'graphqls', 'gql', 'gqls'];
 function handleModule(m, filename) {
   const content = readFileSync(filename, 'utf-8');
 
-  m.exports = parse(content);
+  m.exports = content;
 }
 
 VALID_EXTENSIONS.forEach(ext => {
